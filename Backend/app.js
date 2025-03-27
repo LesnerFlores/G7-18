e3865892c2358bf0a21117cee0609b3b8dab3ed3
IT <lizeth.avila@unah.hn>
2025-03-26 23:03:26 -0600

    App.js como en el commit inicial.

--- Backend/app.js
+++ Backend/app.js
@@ -1,46 +1,31 @@
-const express = require('express');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const restFul = require('express-method-override');
-const routes = require('./routes/router'); // No necesitas la extensión .js
-const cors = require('cors');
+'use strict'
 
-const faviconURL = `${__dirname}/public/img/node-favicon.png`;
-const publicDir = express.static(`${__dirname}/public`);
-const viewDir = `${__dirname}/views`;
-const port = process.env.PORT || 5007;
-const app = express();
-
-// Importa el controlador de clientes (asegúrate de que la ruta sea correcta)
-const { 
-    getAllClientes, 
-    getClienteById, 
-    insertCliente, 
-    updateCliente, 
-    deleteCliente 
-} = require('./controllers/cliente-controller');
-
-// Rutas para los servicios API REST
-app.get('/clientes', getAllClientes);
-app.get('/clientes/:id', getClienteById);
-app.post('/clientes', insertCliente);
-app.put('/clientes/:id', updateCliente);
-app.delete('/clientes/:id', deleteCliente);
+var express = require('express'),
+	favicon = require('serve-favicon'),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	restFul = require('express-method-override')('_method'),
+	routes = require('./routes/router'),
+    cors = require('cors'),
+	faviconURL = `${__dirname}/public/img/node-favicon.png`,
+	publicDir = express.static(`${__dirname}/public`),
+	viewDir = `${__dirname}/views`,
+	port = (process.env.PORT || 5007),
+    app = express()
 
 app
-    .set('views', viewDir)
-    .set('view engine', 'jade')
-    .set('port', port)
-    .use(cors())
-    .use(favicon(faviconURL))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(restFul)
-    .use(morgan('dev'))
-    .use(publicDir)
-    .use(routes);
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+	.set('views', viewDir)
+	.set('view engine', 'jade')
+	.set('port', port)
+	.use(cors())
+	.use( favicon(faviconURL) )
+	// parse application/json
+	.use( bodyParser.json() )
+	// parse application/x-www-form-urlencoded
+	.use( bodyParser.urlencoded({extended: false}) )
+	.use(restFul)
+	.use( morgan('dev') )
+	.use(publicDir)
+	.use(routes)
+	
+module.exports = app
